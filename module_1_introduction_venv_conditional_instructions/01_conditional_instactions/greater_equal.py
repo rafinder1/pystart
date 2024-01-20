857ec9bc5aea3371107cf30374a30242ed20e529
rafinder1 <rafpro33@gmail.com>
2024-01-20 22:39:20 +0100

    checked greater equal and less number

---
+++ module_1_introduction_venv_conditional_instructions/01_conditional_instactions/greater_equal.py
@@ -0,0 +1,9 @@
+number_one = float(input("Give me the first number: "))
+number_two = float(input("Give me the second number: "))
+
+if number_one > number_two:
+    print("The first number is greater than second.")
+elif number_one < number_two:
+    print("The first number is less than second.")
+else:
+    print("The numbers are equal.")

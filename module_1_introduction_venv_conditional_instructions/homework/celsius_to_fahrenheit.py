748a2b431f2fb6703ab9b82af856dadbcfb7a54e
rafinder1 <rafpro33@gmail.com>
2024-01-21 01:02:42 +0100

    added rounded to final temperature

--- module_1_introduction_venv_conditional_instructions/homework/celsius_to_fahrenheit.py
+++ module_1_introduction_venv_conditional_instructions/homework/celsius_to_fahrenheit.py
@@ -2,4 +2,4 @@ celsius = float(input("What is temperature? [C] "))
 
 fahrenheit = celsius * 9 / 5 + 32
 
-print(f"temperature in Fahrenheit is {fahrenheit}")
+print(f"temperature in Fahrenheit is {fahrenheit:.2f}")

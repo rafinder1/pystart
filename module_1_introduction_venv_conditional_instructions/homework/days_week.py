27db9210b110a5efaeb76d4dfd815f64e2cb2c5d
rafinder1 <rafpro33@gmail.com>
2024-01-22 14:05:54 +0100

    defined what day of the week it is

---
+++ module_1_introduction_venv_conditional_instructions/homework/days_week.py
@@ -0,0 +1,18 @@
+number = float(input("Give me a number from 1 to 7: "))
+
+if number < 1 or number > 7:
+    print("Please, define number from 1 to 7.")
+elif number == 1:
+    print("Monday")
+elif number == 2:
+    print("Tuesday")
+elif number == 3:
+    print("Wednesday")
+elif number == 4:
+    print("Thursday")
+elif number == 5:
+    print("Friday")
+elif number == 6:
+    print("Saturday")
+elif number == 7:
+    print("Sunday")

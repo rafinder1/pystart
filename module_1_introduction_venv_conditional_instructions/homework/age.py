9dcdab7c3adc30345aa4fb7042335ec2f7e03620
rafinder1 <rafpro33@gmail.com>
2024-01-22 17:28:06 +0100

    checked if you are an adult and changed conditional in leap year

--- module_1_introduction_venv_conditional_instructions/homework/age.py
+++ module_1_introduction_venv_conditional_instructions/homework/age.py
@@ -4,12 +4,18 @@ current_year = datetime.now().year
 
 birth_year = int(input("What is your year of birth? "))
 
-years_old = int(current_year - birth_year)
+age = int(current_year - birth_year)
 
-print(f"You have {years_old} years old.")
+print(f"You have {age} years old.")
+
+if age >= 18:
+    print("You are an adult")
+else:
+    print(f"You will be an adult in {18 - age} years.")
+
 
 def is_leap_year(year):
-    if year % 4 == 0:
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         print(f"{year} is a leap year.")
     else:
         print(f"{year} is not a leap year.")

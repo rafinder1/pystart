dafdaa62518535a2ee130cbb3a5b52c81d80cc4e
rafinder1 <rafpro33@gmail.com>
2024-01-22 13:54:53 +0100

    find the biggest and smallest number

---
+++ module_1_introduction_venv_conditional_instructions/homework/biggest_smallest_number.py
@@ -0,0 +1,9 @@
+number_one = float(input("Give me a number: "))
+number_two = float(input("Give me a number: "))
+number_three = float(input("Give me a number: "))
+
+max_number = max([number_one, number_two, number_three])
+min_number = min([number_one, number_two, number_three])
+
+print(f"The biggest number is {max_number}")
+print(f"The smallest number is {min_number}")

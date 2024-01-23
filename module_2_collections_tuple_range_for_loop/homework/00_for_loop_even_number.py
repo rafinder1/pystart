b67f6d830164db9bee44b7b82b8e130e2dadffb3
rafinder1 <rafpro33@gmail.com>
2024-01-23 17:01:26 +0100

    added description about tasks with even numbers

--- module_2_collections_tuple_range_for_loop/homework/00_for_loop_even_number.py
+++ module_2_collections_tuple_range_for_loop/homework/00_for_loop_even_number.py
@@ -1,3 +1,7 @@
+# Write a program that will receive an integer from the user,
+# and then it will display all even numbers on the screen
+# from 2 to this number. Display all these numbers separated by commas.
+
 number = int(input("Give me a number: "))
 
 sum_numbers = 0
@@ -6,6 +10,9 @@ for index, element in enumerate(range(2, number + 1, 2)):
     print(element, end=", ")
 
 print("")
+
+# Below, display the sum, number, average, and product of these numbers.
+
 print(f"Sum numbers: {sum_numbers}")
 count_numbers = index + 1
 

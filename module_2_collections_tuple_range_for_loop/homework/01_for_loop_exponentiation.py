c322a805c10581a1a50d46610287d7fdd5fd1d14
rafinder1 <rafpro33@gmail.com>
2024-01-23 17:18:36 +0100

    wrote code with loop which exponentiation by 2, 3, 4, 5 (description)

--- module_2_collections_tuple_range_for_loop/homework/01_for_loop_exponentiation.py
+++ module_2_collections_tuple_range_for_loop/homework/01_for_loop_exponentiation.py
@@ -1,3 +1,7 @@
+# Receive a number from the user and then display it
+# this number raised to the power of 2, 3, 4 and 5.
+# Use a for loop!
+
 number = int(input("Give me a number: "))
 
 exponentiation = 1

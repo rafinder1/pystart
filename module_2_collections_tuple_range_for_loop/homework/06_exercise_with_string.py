f1f8c39292a2d53ab15d3f79fb954534437b842b
rafinder1 <rafpro33@gmail.com>
2024-01-23 18:37:58 +0100

    added exercise with string (description)

--- module_2_collections_tuple_range_for_loop/homework/06_exercise_with_string.py
+++ module_2_collections_tuple_range_for_loop/homework/06_exercise_with_string.py
@@ -1,7 +1,11 @@
 text = input("Give me text: ")
 
+# Write a program that will take any string of characters from the user and print it on the screen
+# in reverse order.
 print(text[::-1])
 
+# Write a program that will take a string of characters from the user and return the same text
+# without spaces and punctuation.
 text = text.replace(" ", "")
 text = text.replace("!", "")
 text = text.replace("?", "")

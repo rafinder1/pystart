2cdfc1f55f6e6cb86f1bb839d1beda9759b50c6b
rafinder1 <rafpro33@gmail.com>
2024-01-23 18:28:38 +0100

    added execise with tuple and for loop (description)

--- module_2_collections_tuple_range_for_loop/homework/05_exercise_with_tuple_and_for_loop.py
+++ module_2_collections_tuple_range_for_loop/homework/05_exercise_with_tuple_and_for_loop.py
@@ -1,5 +1,14 @@
+# Prepare a tuple storing the name, surname and age of the person. Then
+# display this data as:
+# First Name: John
+# Last Name: Smith
+# Age: 40
+
 person_info = "John", "Smith", 40
 abstract_info = "First Name", "Last Name", "Age"
+
+# Declare tuples as a=((1,2,3), (4,5,6), (7,8,9)) and then
+# traversing them using a for loop display:
 
 for i in range(len(person_info)):
     print(f"{abstract_info[i]}: {person_info[i]}")

d7ac7f259d541a62b9903e6e0af8069c0f3312a3
rafinder1 <rafpro33@gmail.com>
2024-01-23 18:53:00 +0100

    added exercise with string and for loop (description)

--- module_2_collections_tuple_range_for_loop/homework/07_exercise_with_string_for_loop.py
+++ module_2_collections_tuple_range_for_loop/homework/07_exercise_with_string_for_loop.py
@@ -1,6 +1,8 @@
 test = "Ala ma kota XD XD LOL"
 
 print("Ex. 3")
+# Receive a sentence from the user and then write it in such a way that individual words are written
+# once in LOWER CAPS and once in CAPITAL letters.
 for index, word in enumerate(test.split(" ")):
     if (index + 1) % 2 == 0:
         print(word.lower())
@@ -8,6 +10,8 @@ for index, word in enumerate(test.split(" ")):
         print(word.upper())
 
 print("Ex. 4")
+# Below, write in lower case only those words from the given sentence that start and end
+# with the same letter.
 for word in test.split(" "):
 
     if word[0].lower() == word[-1].lower():
@@ -15,6 +19,7 @@ for word in test.split(" "):
 
 vowels = 'a', 'ą', 'e', 'ę', 'i', 'o', 'u', 'ó', 'y'
 print("Ex. 5")
+# Below, display all the words in the sentence with the vowels removed.
 for word in test.split(" "):
     for vowel in vowels:
         word = word.lower().replace(vowel, "")

8ba50f008636f29f3d377cfcf8f260c234bd94ba
rafinder1 <rafpro33@gmail.com>
2024-01-23 17:20:29 +0100

    added ascii finder (encoder) - description

--- module_2_collections_tuple_range_for_loop/homework/02_ascii_finder.py
+++ module_2_collections_tuple_range_for_loop/homework/02_ascii_finder.py
@@ -1,3 +1,7 @@
+# Write code that loops through all characters
+# string and will display each character separately and
+# equivalent from an ASCII array using the ord() function
+
 import string
 
 characters = string.ascii_lowercase

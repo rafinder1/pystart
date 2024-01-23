932e51f00172c619a24b96a9db598af4a30baa42
rafinder1 <rafpro33@gmail.com>
2024-01-23 16:58:19 +0100

    added basics statistic numbers: average count, sum

--- module_2_collections_tuple_range_for_loop/homework/00_for_loop_even_number.py
+++ module_2_collections_tuple_range_for_loop/homework/00_for_loop_even_number.py
@@ -1,4 +1,16 @@
 number = int(input("Give me a number: "))
 
-for i in range(2, number + 1, 2):
-    print(i, end=", ")
+sum_numbers = 0
+for index, element in enumerate(range(2, number + 1, 2)):
+    sum_numbers += element
+    print(element, end=", ")
+
+print("")
+print(f"Sum numbers: {sum_numbers}")
+count_numbers = index + 1
+
+print(f"Count number: {count_numbers}")
+
+print(f"Average numbers: {sum_numbers / count_numbers}")
+
+print(f"Product of numbers: {sum_numbers * count_numbers}")

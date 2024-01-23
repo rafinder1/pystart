e0813e5f81934b36160e0190c2b6dec91de48f2a
rafinder1 <rafpro33@gmail.com>
2024-01-23 19:09:12 +0100

    Ex. 01 changed name final result

--- module_2_collections_tuple_range_for_loop/homework/01_for_loop_exponentiation.py
+++ module_2_collections_tuple_range_for_loop/homework/01_for_loop_exponentiation.py
@@ -4,9 +4,9 @@
 
 number = int(input("Give me a number: "))
 
-exponentiation = 1
+result = 1
 for i in [2, 3, 4, 5]:
     for j in range(0, i):
-        exponentiation *= number
-    print(exponentiation)
-    exponentiation = 1
+        result *= number
+    print(result)
+    result = 1

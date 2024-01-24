8400a1a2e03608ace3111dc7bb318fd49685fc9c
rafinder1 <rafpro33@gmail.com>
2024-01-24 08:40:34 +0100

    chenged method in ex. 07

--- module_2_collections_tuple_range_for_loop/homework/07_exercise_with_string_for_loop.py
+++ module_2_collections_tuple_range_for_loop/homework/07_exercise_with_string_for_loop.py
@@ -3,11 +3,14 @@ test = "Ala ma kota XD XD LOL"
 print("Ex. 3")
 # Receive a sentence from the user and then write it in such a way that individual words are written
 # once in LOWER CAPS and once in CAPITAL letters.
+sentence_ex_3 = ''
 for index, word in enumerate(test.split(" ")):
-    if (index + 1) % 2 == 0:
-        print(word.lower())
-    else:
-        print(word.upper())
+    new_word = word.upper()
+    if index % 2 == 0:
+        new_word = word.lower()
+
+    sentence_ex_3 = f'{sentence_ex_3} {new_word}'
+print(sentence_ex_3)
 
 print("Ex. 4")
 # Below, write in lower case only those words from the given sentence that start and end

e2f3f81360369bd929d058dcf65310dbd5501d26
rafinder1 <rafpro33@gmail.com>
2024-01-24 08:35:23 +0100

    changed method which find only numeric and alpha

--- module_2_collections_tuple_range_for_loop/homework/06_exercise_with_string.py
+++ module_2_collections_tuple_range_for_loop/homework/06_exercise_with_string.py
@@ -6,8 +6,8 @@ print(text[::-1])
 
 # Write a program that will take a string of characters from the user and return the same text
 # without spaces and punctuation.
-text = text.replace(" ", "")
-text = text.replace("!", "")
-text = text.replace("?", "")
-
-print(f"Text without spaces and punctuation {text}")
+sentence = ''
+for char in text:
+    if char.isnumeric() or char.isalpha():
+        sentence += char
+print(f"Text without spaces and punctuation {sentence}")

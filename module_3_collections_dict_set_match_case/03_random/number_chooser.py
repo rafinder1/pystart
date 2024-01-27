7a0a9a0fd5cb1745b936b47590e14f822ca0be74
rafinder1 <rafpro33@gmail.com>
2024-01-28 00:00:38 +0100

    renamed number pow

--- module_3_collections_dict_set_match_case/03_random/number_chooser.py
+++ module_3_collections_dict_set_match_case/03_random/number_chooser.py
@@ -3,12 +3,12 @@ import random
 
 number = random.randint(1, 100)
 
-number_exp = math.pow(number, 2)
+number_pow = math.pow(number, 2)
 number_sqrt = math.sqrt(number)
 
 number_floor = math.floor(number_sqrt)
 number_ceil = math.ceil(number_sqrt)
 
 print(f"Your number is {number}")
-print("Exp: ", number_exp, "Sqrt: ", number_sqrt)
+print("Exp: ", number_pow, "Sqrt: ", number_sqrt)
 print("Floor: ", number_floor, "Ceil: ", number_ceil)

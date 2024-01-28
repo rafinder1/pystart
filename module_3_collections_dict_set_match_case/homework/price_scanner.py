4d3ef374c8a8f47af45915d1ad87f42c4e43294f
rafinder1 <rafpro33@gmail.com>
2024-01-28 12:20:15 +0100

    added price scanner (only fruit)

---
+++ module_3_collections_dict_set_match_case/homework/price_scanner.py
@@ -0,0 +1,12 @@
+fruits = {
+    'apple': 1.50,
+    'mango': 10.99,
+    'peach': 6.85,
+    'plum': 5.85,
+    'strawberry': 4.57,
+    'blueberry': 6.87,
+    'blackberry': 9.23,
+    'banana': 6.99
+}
+
+print("Price apple: ", fruits["apple"], "Price banana: ",fruits['banana'])

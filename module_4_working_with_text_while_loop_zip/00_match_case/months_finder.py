d88f1bde4868618c277f648644543cd678480c33
rafinder1 <rafpro33@gmail.com>
2024-01-29 21:46:04 +0100

    added examples with match case and try except

---
+++ module_4_working_with_text_while_loop_zip/00_match_case/months_finder.py
@@ -0,0 +1,49 @@
+months = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
+}
+
+number = int(input("Give me number month: "))
+
+try:
+    print(months[number])
+except KeyError:
+    print("This month do not exist!")
+
+match number:
+    case 1:
+        print("January")
+    case 2:
+        print("February")
+    case 3:
+        print("March")
+    case 4:
+        print("April")
+    case 5:
+        print("May")
+    case 6:
+        print("June")
+    case 7:
+        print("July")
+    case 8:
+        print("August")
+    case 9:
+        print("September")
+    case 10:
+        print("October")
+    case 11:
+        print("November")
+    case 12:
+        print("December")
+    case _:
+        print("This month do not exist!")
